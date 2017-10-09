696de3d6326d5cc4bb6a23cfe8a0ce6572d4d80c
Kyle Lim <kylelim8998@gmail.com>
2017-10-08 23:21:24 -0400

    Initial files created!
    Nothing is functional at all yet. Borrowing some elements from my web
    sockets chat application.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Vynchronize
-Watch synchronized videos with others online
+Created by Kyle Lim
+
+Watch synchronized videos with others online!
+
+This is a work in progress. It is not functional at all yet!
+
+### Dependencies
+
+Socket.io
+
+Node.js
+
+### How to run locally
+
+Run the server
+```
+node server
+```
+
+Access the page by going to localhost:3000
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server Started . . .');
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+    // Connect Socket
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    // Disconnect
+    socket.on('disconnect', function(data){
+        // if(!socket.username) return;
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    // // Send Message in chat
+    // socket.on('send message', function(data){
+    //     // console.log(data);
+    //     io.sockets.emit('new message', {msg: data, user: socket.username});
+    // });
+
+    // New User
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
+});
